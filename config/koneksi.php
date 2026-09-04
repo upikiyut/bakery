f71764f2f1cbb40a49a4c31e0dbf1c5fdca352b2
upikiyut <secondbeljr@gmail.com>
2026-09-04 10:54:09 +0700

    Update koneksi.php

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,12 +1,16 @@
 <?php
+date_default_timezone_set('Asia/Jakarta');
+
 $host = getenv('MYSQLHOST') ?: 'localhost';
 $port = getenv('MYSQLPORT') ?: '3306';
-$db   = getenv('MYSQLDATABASE') ?: 'bunea_bakery';
+$db   = getenv('MYSQLDATABASE') ?: 'railway';
 $user = getenv('MYSQLUSER') ?: 'root';
 $pass = getenv('MYSQLPASSWORD') ?: '';
 
 $conn = new mysqli($host, $user, $pass, $db, $port);
-
 if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
 }
+
+$conn->set_charset("utf8mb4");
+$conn->query("SET time_zone = '+07:00'");
